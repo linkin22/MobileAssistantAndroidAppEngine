2ff9ec8e28d859c84b3fb82864c6632087fe2c59
linkin <o.ramogi@gmail.com>
2015-10-15 17:16:01 +0300

    Initial commit

---
+++ android/build/generated/source/buildConfig/androidTest/debug/com/google/sample/mobileassistant/test/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.sample.mobileassistant.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.sample.mobileassistant.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final float DUMMY_LATITUDE = 37.22222f;
+  public static final float DUMMY_LONGITUDE = -122.11111f;
+  public static final String ROOT_URL = "http://10.0.2.2:8080/_ah/api/";
+  public static final String SENDER_ID = "208926334599";
+  public static final boolean SIGN_IN_REQUIRED = false;
+  public static final String WEB_CLIENT_ID = "208926334599-og3fhe3a2cl8chgej77o74tlbhqs5vp6.apps.googleusercontent.com";
+}
